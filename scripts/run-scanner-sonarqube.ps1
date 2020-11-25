26daa13ca2b4f597f8aad91eca270652b71e56c6
Wilzon Liscano <wliscano@ventasyservicios.com.co>
2020-11-24 21:25:14 -0500

    Check Pipeline

--- scripts/run-scanner-sonarqube.ps1
+++ scripts/run-scanner-sonarqube.ps1
@@ -5,7 +5,7 @@ $path = "tests\CodeDesignPlus.EFCore.Test"
 $project = "$path\CodeDesignPlus.EFCore.Test.csproj"
 $report = "$path\coverage.opencover.xml"
 
-# Run Sonnar Scanner 
+# Run Sonnar Scanner  
 dotnet test $project /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:Exclude="[CodeDesignPlus.Entities]*%2c[CodeDesignPlus.InMemory]*%2c[CodeDesignPlus.Abstractions]*"
 
 dotnet sonarscanner begin /k:"CodeDesignPlus.EFCore" /d:sonar.host.url=http://localhost:9000 /d:sonar.cs.opencover.reportsPaths="$report" /d:sonar.coverage.exclusions="**/CodeDesignPlus.Abstractions/**/*.cs,**/CodeDesignPlus.Entities/**/*.cs,**/CodeDesignPlus.InMemory/**/*.cs,**Test*.cs"
