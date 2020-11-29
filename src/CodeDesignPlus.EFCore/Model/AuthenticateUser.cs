2dd0306686cd7ef40599b10162b8f0f1e5fc515c
Wilzon Liscano <wliscano@ventasyservicios.com.co>
2020-11-29 14:11:06 -0500

    Se agrega la documentación de los metodos

---
+++ src/CodeDesignPlus.EFCore/Model/AuthenticateUser.cs
@@ -0,0 +1,26 @@
+﻿namespace CodeDesignPlus.EFCore.Model
+{
+    /// <summary>
+    /// Default implementation of IAuthenticateUser<typeparamref name="TKeyUser"/>
+    /// </summary>
+    /// <typeparam name="TKeyUser">Type of data that the user will identify</typeparam>
+    public class AuthenticateUser<TKeyUser>: IAuthenticateUser<TKeyUser>
+    {
+        /// <summary>
+        /// Gets a value boolean that indicates whether is a application
+        /// </summary>
+        public bool IsApplication { get; set; }
+        /// <summary>
+        /// Gets the Id User authenticated
+        /// </summary>
+        public TKeyUser IdUser { get; set; }
+        /// <summary>
+        /// Gets a value that indicates whether the user has been authenticated.
+        /// </summary>
+        public bool IsAuthenticated { get; set; }
+        /// <summary>
+        /// Gets the name of the current user.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
---
+++ src/CodeDesignPlus.EFCore/Operations/IOperation.cs
@@ -0,0 +1,17 @@
+﻿using CodeDesignPlus.Core.Abstractions;
+
+namespace CodeDesignPlus.EFCore.Operations
+{
+    /// <summary>
+    /// Enables standardized CRUD operations in the SDK
+    /// </summary>
+    /// <typeparam name="TKey">Type of data that will identify the record</typeparam>
+    /// <typeparam name="TUserKey">Type of data that the user will identify</typeparam>
+    /// <typeparam name="TEntity">The entity type to be configured.</typeparam>
+    public interface IOperation<TKey, TUserKey, TEntity> :
+        ICreateOperation<TKey, TUserKey, TEntity>,
+        IUpdateOperation<TKey, TUserKey, TEntity>,
+        IDeleteOperation<TKey, TUserKey, TEntity>
+        where TEntity : class, IEntityBase<TKey, TUserKey>
+    { }
+}
