80afd8deed89a3ae5b12e29551c4a25fcfaf5c91
Wilzon Liscano <wliscano@ventasyservicios.com.co>
2021-01-04 21:36:59 -0500

    Se actualizan el diagrama y la versión

--- samples/api/CodeDesignPlus.EFCore.Sample.Api/Program.cs
+++ samples/api/CodeDesignPlus.EFCore.Sample.Api/Program.cs
@@ -1,5 +1,11 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CodeDesignPlus.EFCore.Sample.Api
 {
